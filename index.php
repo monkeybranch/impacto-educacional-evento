8c7c7bd499aa0716bd0b46f9d1b864d08cc0e560
João Batista <joaobgj@gmail.com>
2021-02-01 07:31:15 -0300

    HTML Slider

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+	/*
+		Criação: Slider Convite by MonkeyBranch.dev
+		Data: Primeiro semestre de 2021
+		Conteudo: HTML de estrutura para o slider de informações do evento
+	*/
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Convite - Impacto Educacional</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="icon" href="web-root/imagens/estrutura/favicon.png" sizes="16x16" type="image/png">
+		
+		<!-- CSS -->
+		<link rel="stylesheet" href="web-root/css/estrutura.css">
+	</head>
+	<body>
+		<div id="DIVSlider" class="DIVSlider">
+			<div class="DIVSliderCaixa">
+				<div id="DIVSlides" class="DIVSlides">
+					<span class="SPANSlide">ENTRADA</span>
+					<span class="SPANSlide">AUTOR 1</span>
+					<span class="SPANSlide">AUTOR 2</span>
+					<span class="SPANSlide">AUTOR 3</span>
+					<span class="SPANSlide">AUTOR 4...</span>
+				</div>
+			</div>
+		</div>
+
+		<!-- JAVASCRIPT -->
+		<script src="web-root/javascript/estrutura.js" type="text/javascript"></script>
+	</body>
+</html>
