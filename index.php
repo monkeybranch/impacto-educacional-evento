936d14a59df3da3631f84a63b0ef79ed28b8b511
João Batista <joaobgj@gmail.com>
2021-02-02 19:10:54 -0300

    Adicionado favicon final

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 		<meta charset="UTF-8">
 		<title>Convite - Impacto Educacional</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="icon" href="web-root/imagens/estrutura/favicon.png" sizes="16x16" type="image/png">
+		<link rel="icon" href="web-root/imagens/favicon.png" sizes="16x16" type="image/png">
 		
 		<!-- CSS -->
 		<link rel="stylesheet" href="web-root/css/estrutura.css">
